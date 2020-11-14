ea1a178c9bf6bc4ddc882d427009c700ac4b67c9
Arkadiusz Pasek <thevik444@gmail.com>
2020-11-14 23:16:07 +0100

    Updates

---
+++ precache-manifest.f53d16b154b359387bfd3521d21897d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ce9d35dcebc2ac8f616a58a4853421",
+    "url": "/stock-research/index.html"
+  },
+  {
+    "revision": "fc1cb6489a385cb0764f",
+    "url": "/stock-research/static/js/2.2a5efeea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stock-research/static/js/2.2a5efeea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d60478d46e20aaf4a4d7",
+    "url": "/stock-research/static/js/main.7ae784e1.chunk.js"
+  },
+  {
+    "revision": "66de14ca903b84eff237",
+    "url": "/stock-research/static/js/runtime-main.fc85dd99.js"
+  }
+]);
